54cecd3310f81d1312c7c3d32b24703249152ed8
Cristian Montes <cmontesmujica@gmail.com>
2021-08-21 20:01:07 -0700

    winner and looser showing

--- src/index.js
+++ src/index.js
@@ -20,8 +20,13 @@ function Square(props){
           squares: Array(9).fill(null), 
           xIsNext: true,
         }
+
         handleClick(i) {
             const squares = this.state.squares.slice();
+            if (calculateWinner(squares) || squares[i]){
+                return;
+            } 
+
             squares[i] = this.state.xIsNext ? 'X' : 'O';
             this.setState({
                 squares: squares,
@@ -30,16 +35,23 @@ function Square(props){
         }
     
     renderSquare(i) {
-      return < Square 
+      return ( 
+            < Square 
             value={this.state.squares[i]}  
             onClick={() => this.handleClick(i)}
-            />;
+            />
+      );
     }
 
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-  
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if(winner) {
+            status = 'Winner:' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
       return (
         <div>
           <div className="status">{status}</div>
@@ -79,6 +91,27 @@ function Square(props){
     }
   }
   
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   // ========================================
   
   ReactDOM.render(
